0cb7ec9b801aa5881e484888c0470ed5cb65fc8e
ivan <107969090+free-hornet@users.noreply.github.com>
2024-04-30 17:45:52 -0400

    tabla de ranking

--- sentencias_ayudas.sql
+++ sentencias_ayudas.sql
@@ -18,3 +18,16 @@ JOIN public.vendedor vd ON v.fk_id_vendedor = vd.pk_id
 JOIN public.empleado e ON vd.fk_rut_empleado = e.pk_rut
 JOIN tienda_empleado te ON e.pk_rut = te.fk_rut_empleado
 group by te.fk_id_tienda, p.pk_id;
+
+-- Tabla que hace un ranking a cada producto por cada tienda, 1 al o los más caro/s
+SELECT id_tienda, id_producto, nombre_producto, precio, RANK() OVER (PARTITION BY id_tienda ORDER BY precio DESC) AS ranking
+	FROM (
+		select te.fk_id_tienda as id_tienda, p.pk_id AS id_producto, p.nombre AS nombre_producto, p.precio 
+		FROM public.producto p
+		JOIN public.producto_venta pv ON p.pk_id = pv.fk_id_producto
+		JOIN public.venta v ON pv.fk_id_venta = v.pk_id
+		JOIN public.vendedor vd ON v.fk_id_vendedor = vd.pk_id
+		JOIN public.empleado e ON vd.fk_rut_empleado = e.pk_rut
+		JOIN public.tienda_empleado te ON e.pk_rut = te.fk_rut_empleado
+		group by te.fk_id_tienda, p.pk_id
+	);
